a6c5f9b659072b72ae3f6604f1fc22f2f4b5a88a
wagonlips <sean@seancamden.com>
2019-02-20 20:49:45 -0800

    Removed search bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -32,7 +32,6 @@ const NavBar = ({toggleNavbar, isActive}) => (
         <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id='navMenu'>
 
           <div className='navbar-end'>
-            <SearchBox searchIndex={data.siteSearchIndex.index} />
             <Link className='navbar-item' to='/about'>
                             About
             </Link>
