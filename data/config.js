33967084adc430d39fc09b5e9e49877f524cd446
wagonlips <sean@seancamden.com>
2019-02-21 17:00:53 -0800

    Updated site info data/config.js

--- data/config.js
+++ data/config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'The Galley Santa Rosa', // Site title.
+  siteTitleAlt: 'Cannabis Contract Manufacturing & Distribution', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Edibles, Tinctures/Topicals, Hard Candy/Gummies, Chocolate, Beverages, Vapes/Flower/Pre-Rolls', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  disqusShortname: 'thegalleysr', // Disqus shortname.
+  userName: 'thegalleysr',
+  userTwitter: 'thegalleysr',
+  userLocation: 'Santa Rosa, California, USA',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  copyright: 'Copyright © The Galley Santa Rosa 2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#000000', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
